99d1f6535ad06851bae751ce549ca87dc9a48d79
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2017-06-06 14:16:52 +0100

    Specific ability

--- lib/overwatch/ability.rb
+++ lib/overwatch/ability.rb
@@ -1,4 +1,7 @@
 module Overwatch
   class Ability < Hashie::Mash
+    def hero
+      Hero.new(Client.new.ability(id)[:hero])
+    end
   end
 end
--- spec/lib/overwatch/ability_spec.rb
+++ spec/lib/overwatch/ability_spec.rb
@@ -24,4 +24,40 @@ describe Overwatch::Hero do
     subject { ability.is_ultimate }
     it { is_expected.to eq false }
   end
+
+  describe "#hero" do
+    let(:hero) { ability.hero }
+
+    before do
+      stub_request(:get, "https://overwatch-api.net/api/v1/ability/1").
+        to_return(status: 200, body: File.read("spec/fixtures/overwatch/ability.json"))
+      stub_request(:get, "https://overwatch-api.net/api/v1/hero/1").
+        to_return(status: 200, body: File.read("spec/fixtures/overwatch/hero.json"))
+    end
+
+    describe "#id" do
+      subject { hero.id }
+      it { is_expected.to eq 1 }
+    end
+
+    describe "#real_name" do
+      subject { hero.real_name }
+      it { is_expected.to eq "Ana Amari" }
+    end
+
+    describe "#health" do
+      subject { hero.health }
+      it { is_expected.to eq 200 }
+    end
+
+    describe "#armour" do
+      subject { hero.armour }
+      it { is_expected.to eq 0 }
+    end
+
+    describe "#abilities" do
+      subject { hero.abilities.size }
+      it { is_expected.to eq(4) }
+    end
+  end
 end
